4dcc56a096ddf05c1648c226b81c9c18a7a985d2
Jin Lee <leejin21c@gmail.com>
2020-12-17 21:41:51 +0900

    complete game

--- src/index.js
+++ src/index.js
@@ -2,34 +2,57 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 
-class Square extends React.Component {
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => {
-                    alert("click!");
-                    // onClick = {alert('click')}
-                    // 이면 해당 컴포를 render할 때마다 alert를 호출
-                    // ? 컴포가 render할 때 alert()의 "결과"를 onClick에 바인딩하기 때문 => this와 연관되어 있어서 arrow ft을 쓰는 게 좋음.
-                }}
-            >
-                {this.props.value}
-            </button>
-        );
+const Square = (props) => {
+    // controlled components
+    return (
+        <button
+            className="square"
+            onClick={() => {
+                props.onClick();
+            }}
+        >
+            {props.value}
+        </button>
+    );
+};
+
+const calculateWinner = (squares) => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+        ) {
+            return squares[a];
+        }
     }
-}
+    return null;
+};
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
+            />
+        );
     }
 
     render() {
-        const status = "Next Player : X";
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -51,14 +74,55 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [
+                {
+                    squares: Array(9).fill(null),
+                },
+            ],
+            XisNext: true,
+        };
+    }
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        // 어떤 배열의 begin부터 end까지(end 미포함)에 대한 얕은 복사본을 새로운 배열 객체로 반환합니다. 원본 배열은 바뀌지 않습니다.
+        // https://www.notion.so/Why-Immutability-is-important-542e3dca878b409dab0af5a9ec09ff6b
+
+        if (calculateWinner(squares)) {
+            return;
+        }
+
+        squares[i] = this.state.XisNext === true ? "X" : "O";
+        this.setState({
+            history: history.concat([{ squares }]),
+            // concat does not mutate the original object
+            XisNext: !this.state.XisNext,
+        });
+    }
     render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = calculateWinner(current.squares);
+        let status;
+        if (winner) {
+            status = "Winner : " + winner;
+        } else {
+            status = "Next Player : " + (this.state.XisNext ? "X" : "O");
+        }
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board></Board>
+                    <Board
+                        onClick={(i) => this.handleClick(i)}
+                        squares={current.squares}
+                    ></Board>
                 </div>
                 <div className="game-info">
-                    <div>{/* Status */}</div>
+                    <div className="status">{status}</div>
                     <div>{/* TODO */}</div>
                 </div>
             </div>
